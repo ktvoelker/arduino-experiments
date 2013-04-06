123c1d60e95081c121db055e349f4df0000f0ebd
Karl Voelker <ktvoelker@gmail.com>
2013-04-05 22:20:21 -0700

    Added some structure to reuse the core make logic

--- build/uno.mk
+++ build/uno.mk
@@ -17,28 +17,32 @@ SIZEOPTS=-Os -Wl,--gc-sections -ffunction-sections -fdata-sections
 DUDECONF=$(ARDUINO)/hardware/tools/avr/etc/avrdude.conf
 DUDEOPTS=-C $(DUDECONF) -c arduino -p m328p -b 115200 -P $(DEVICE) 
 
-.PHONY: all clean upload
+.PHONY: all clean flash
 
-all: test.hex
+PROGRAM=main.hex
 
-upload: test.hex
-	$(DUDE) $(DUDEOPTS) -U flash:w:test.hex
+SOURCES=$(wildcard *.c)
 
-test.hex: test.elf
-	$(OBJCOPY) -O ihex -R .eeprom test.elf test.hex
+all: $(PROGRAM)
 
-test.elf: test.c
+flash: $(PROGRAM)
+	$(DUDE) $(DUDEOPTS) -U flash:w:$(PROGRAM)
+
+%.hex: %.elf
+	$(OBJCOPY) -O ihex -R .eeprom $*.elf $*.hex
+
+%.elf: $(SOURCES)
 	$(GCC) \
 		-isystem $(INCCORE) \
 		-isystem $(INCVAR) \
 		-isystem $(INCTOOLS) \
 		-D __AVR_ATmega328P__ \
-		-o test.elf \
+		-o $*.elf \
 		$(OPTS) \
 		$(SIZEOPTS) \
-		test.c \
-		core.a
+		$(SOURCES) \
+		../build/uno.a
 
 clean:
-	rm test.hex test.elf
+	-rm *.hex *.elf
 
